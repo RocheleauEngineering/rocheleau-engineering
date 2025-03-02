1d6ad69f5cdc9aaa4e60dd6e7283918c86fdf2b2
RocheleauEngineering <carmina@rocheleau-engineering.com>
2025-03-02 15:37:08 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function showMessage() { 
+    // Defines a function named `showMessage` that will be executed when the button is clicked.
+    
+    document.getElementById("message").innerText = "Hello, world!"; 
+    // Finds the paragraph with ID "message" and changes its text content to "Hello, world!".
+}
